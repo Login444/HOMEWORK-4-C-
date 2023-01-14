12798b7a5051f1c2d6d3b59aa72bd9fc7be66c3d
login4 <birukovartemij@gmail.com>
2023-01-14 16:50:22 +0300

    TASK3

---
+++ TASK3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Укажите колличество элементов массива: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[] array = GetBinaryArray(b);
+Console.WriteLine($" [ {String.Join(" , ", array)} ] ");
+
+
+int[] GetBinaryArray(int size) // GetBinaryArray - это просто название функции
+
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(19);
+    }
+
+    return result;
+}
