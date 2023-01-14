1f935b3698e987a107bf6cfd0b1c58ce1ff6205e
login4 <birukovartemij@gmail.com>
2023-01-14 16:39:24 +0300

    TASK2 completed

---
+++ TASK2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int summa (int number)
+{
+    int sum = 0;
+    int fig = 0;
+    while (number >0)
+    {
+        fig = number % 10;
+        number = number / 10;
+        sum = sum + fig;
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма цифр числа {num} равна {summa(num)}");
