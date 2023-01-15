57c21cbb6d9d1d72b1c6cbc6e0409276bff122b7
login4 <birukovartemij@gmail.com>
2023-01-15 10:31:47 +0300

    все сделано

--- TASK3/Program.cs
+++ TASK3/Program.cs
@@ -11,7 +11,7 @@ int[] GetArray(int size)
     int[] result = new int[size];
     for (int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(33);
+        result[i] = new Random().Next(0, 33 + 1);
     }
 
     return result;
